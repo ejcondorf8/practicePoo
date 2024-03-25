94bb51906ff0061a06ff2d825fc8f789f0625628
ejcondorf8 <ejcondorf@uce.edu.ec>
2024-03-25 10:18:26 -0700

    Creatcion object orden for finaltest

---
+++ app/orden.py
@@ -0,0 +1,18 @@
+from computadora import Computer
+from functools import reduce
+class Orden():
+    countOrden = 0
+    @classmethod
+    def nextVal(cls):
+        cls.countOrden += 1
+        return cls.countOrden
+    def __init__(self,computers:Computer) -> None:
+        self._id=Orden.nextVal()
+        self._computers=list(computers)
+    @property
+    def computers(self):
+        return self._computers
+    def getTotalPrice(self):
+        listAux=list(reduce(lambda x,y: x+y,self.computers))
+        return listAux
+        
