e190ff4c52bafae6bbd8568cafaa66c92159d18b
ejcondorf8 <ejcondorf@uce.edu.ec>
2024-03-26 07:21:48 -0700

    Fix Bug in orden and new functicion

--- app/orden.py
+++ app/orden.py
@@ -1,5 +1,8 @@
 from computadora import Computer
 from functools import reduce
+from monitor import Monitor
+from raton import Raton
+from teclado import Teclado
 class Orden():
     countOrden = 0
     @classmethod
@@ -13,4 +16,9 @@ class Orden():
     def computers(self):
         return self._computers
     def getTotalPrice(self):
-        print(self._computers)
+        listAux= [computer.price for computer in self._computers]
+        print(listAux)
+        return reduce(lambda a,b:a+b,listAux)
+    
+    def addComputer(self,computer:Computer):
+        self._computers.append(computer)
